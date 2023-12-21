3ef46c38663cf8e8ade54e55a5b14f265b0e7ebf
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-21 23:28:39 +0530

    Update font loading and header style in Layout component

--- app/_layout.js
+++ app/_layout.js
@@ -1,33 +1,59 @@
-import { Text, View } from 'react-native';
 import {
-  SplashScreen,
-  // This example uses a basic Layout component, but you can use any Layout.
-  Slot,
-  Stack,
+    SplashScreen,
+    // This example uses a basic Layout component, but you can use any Layout.
+    Slot,
+    Stack,
 } from 'expo-router';
-import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter';
+import { useFonts, 
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black, } from '@expo-google-fonts/inter';
+import { useEffect } from 'react';
 
 
 SplashScreen.preventAutoHideAsync();
 
 
 export default function Layout() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter_500Medium,
-  });
+    const [fontsLoaded, fontError] = useFonts({
 
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
-      SplashScreen.hideAsync();
+        Inter_100Thin,
+        Inter_200ExtraLight,
+        Inter_300Light,
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold,
+        Inter_800ExtraBold,
+        Inter_900Black,
+    });
+
+    useEffect(() => {
+        if (fontsLoaded || fontError) {
+            // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    // Prevent rendering until the font has loaded or an error was returned
+    if (!fontsLoaded && !fontError) {
+        return null;
     }
-  }, [fontsLoaded, fontError]);
 
-  // Prevent rendering until the font has loaded or an error was returned
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
-  // Render the children routes now that all the assets are loaded.
-  return <Stack />;
+    // Render the children routes now that all the assets are loaded.
+    return <Stack
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: '#fff',
+                shadowColor: 'transparent',
+            },
+            headerTintColor: '#000',
+        }}
+    />;
 }
---
+++ constants/theme.js
@@ -0,0 +1,63 @@
+const APP_NAME = "My App";
+const APP_VERSION = "1.0.0";
+const APP_DESCRIPTION = "My App Description";
+const APP_AUTHOR = "My Name";
+
+const COLORS = {
+  primary: "#312651",
+  secondary: "#444262",
+  tertiary: "#FF7754",
+
+  gray: "#83829A",
+  gray2: "#C1C0C8",
+
+  white: "#F3F4F8",
+  lightWhite: "#FAFAFC",
+};
+
+const FONT = {
+  thin: "Inter_100Thin",
+  extraLight: "Inter_200ExtraLight",
+  light: "Inter_300Light",
+  regular: "Inter_400Regular",
+  medium:"Inter_500Medium",
+  semiBold: "Inter_600SemiBold",
+  bold: "Inter_700Bold",
+  extraBold: "Inter_800ExtraBold",
+  black: "Inter_900Black",
+  
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
