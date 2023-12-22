71e74282382f8ba5e623ae5ec204a244aa268b9c
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-22 21:08:17 +0530

    Refactor Paginator component to use animated values for dot styling

--- components/Paginator.js
+++ components/Paginator.js
@@ -15,8 +15,18 @@ const Paginator = ({data, scrollX}) => {
                 outputRange: [8, 16, 8],
                 extrapolate: 'clamp'
             })
+            const opacity = scrollX.interpolate({
+                inputRange,
+                outputRange: [0.3, 1, 0.3],
+                extrapolate: 'clamp'
+            })
+            const backgroundColor = scrollX.interpolate({
+                inputRange,
+                outputRange: [COLORS.gray, COLORS.primary, COLORS.gray],
+                extrapolate: 'clamp'
+            })
           return (
-            <Animated.View key={i} style={{width: 8, height: 8, borderRadius: 4, backgroundColor: COLORS.gray, margin: 4}} />
+            <Animated.View key={i} style={{width: dotWidth, opacity:opacity, height: 8, borderRadius: 4, backgroundColor: backgroundColor, margin: 4}} />
           )
         })
       }
